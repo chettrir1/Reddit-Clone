b2446556195530b33db1140505a8927ba47af312
Raju Chettri <dada@Rajus-MacBook-Pro.local>
2024-02-24 12:53:33 -0500

    palette file added

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_clone/theme/palette.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reddit Clone',
+      theme: Palette.darkModeAppTheme,
+      home: const SizedBox(),
+    );
+  }
+}
