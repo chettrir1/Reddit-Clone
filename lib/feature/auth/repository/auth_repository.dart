fa25632d7358019e126b63ce88220d222c1ed339
Raju Chettri <dada@Rajus-MBP.ht.home>
2024-02-24 17:00:24 -0500

    repository updated

--- lib/feature/auth/repository/auth_repository.dart
+++ lib/feature/auth/repository/auth_repository.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:reddit_clone/core/constants/assets_constants.dart';
 import 'package:reddit_clone/core/constants/constants.dart';
+import 'package:reddit_clone/core/failure.dart';
 import 'package:reddit_clone/core/providers/firebase_provider.dart';
+import 'package:reddit_clone/core/type_defs.dart';
 import 'package:reddit_clone/models/user_model.dart';
 
 final authRepositoryProvider = Provider((ref) => AuthRepository(
@@ -31,7 +33,7 @@ class AuthRepository {
         _firestore = firestore,
         _googleSignIn = googleSignIn;
 
-  void signInWithGoogle() async {
+  FutureEither<UserModel> signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
       final googleAuth = await googleUser?.authentication;
@@ -55,9 +57,19 @@ class AuthRepository {
           awards: [],
         );
         await _users.doc(model.uid).set(model.toMap());
+      } else {
+        model = await getUserData(userCredential.user?.uid ?? "").first;
       }
+      return right(model);
+    } on FirebaseException catch (e) {
+      throw e.message ?? "Something went wrong!";
     } catch (e) {
-      print(e);
+      return left(Failure(e.toString()));
     }
+  }
+
+  Stream<UserModel> getUserData(String uid) {
+    return _users.doc(uid).snapshots().map(
+        (event) => UserModel.fromMap(event.data() as Map<String, dynamic>));
   }
 }
