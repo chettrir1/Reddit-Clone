1c1b6df3154226cbfa6a1d4bdc0e8ff20ccb04fa
Raju Chettri <dada@Rajus-MacBook-Pro.local>
2024-02-26 13:12:36 -0500

    file picker added and image picker added for banner and profile

--- lib/feature/community/screen/edit_community_screen.dart
+++ lib/feature/community/screen/edit_community_screen.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/common/error_text.dart';
 import 'package:reddit_clone/core/common/loader.dart';
 import 'package:reddit_clone/core/constants/constants.dart';
+import 'package:reddit_clone/core/utils.dart';
 import 'package:reddit_clone/feature/community/controller/community_controller.dart';
 import 'package:reddit_clone/theme/palette.dart';
 
@@ -17,6 +22,27 @@ class EditCommunityScreen extends ConsumerStatefulWidget {
 }
 
 class _EditCommunityScreenState extends ConsumerState<EditCommunityScreen> {
+  File? bannerFile;
+  File? profileFile;
+
+  void selectBannerImage() async {
+    final result = await pickImage();
+    if (result != null) {
+      setState(() {
+        bannerFile = File(result.files.first.path ?? "");
+      });
+    }
+  }
+
+  void selectProfileImage() async {
+    final result = await pickImage();
+    if (result != null) {
+      setState(() {
+        profileFile = File(result.files.first.path ?? "");
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ref.watch(getCommunityByNameProvider(widget.name)).when(
@@ -35,29 +61,65 @@ class _EditCommunityScreenState extends ConsumerState<EditCommunityScreen> {
                       ))
                 ],
               ),
-              body: Column(
-                children: [
-                  DottedBorder(
-                      radius: const Radius.circular(4),
-                      dashPattern: const [10, 4],
-                      strokeCap: StrokeCap.round,
-                      color: Palette.darkModeAppTheme.textTheme.bodyMedium!.color!,
-                      child: Container(
-                        width: double.infinity,
-                        height: 150,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10)),
-                        child: (community.banner.isEmpty ||
-                                community.banner ==
-                                    AssetsConstants.bannerDefault)
-                            ? const Center(
-                                child: Icon(
-                                Icons.camera_alt_outlined,
-                                size: 40,
-                              ))
-                            : Image.network(community.banner),
-                      ))
-                ],
+              body: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 200,
+                      child: Stack(
+                        children: [
+                          GestureDetector(
+                            onTap: selectBannerImage,
+                            child: DottedBorder(
+                                radius: const Radius.circular(4),
+                                dashPattern: const [10, 4],
+                                strokeCap: StrokeCap.round,
+                                borderType: BorderType.RRect,
+                                color: Palette.darkModeAppTheme.textTheme
+                                    .bodyMedium!.color!,
+                                child: Container(
+                                  width: double.infinity,
+                                  height: 150,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: (bannerFile != null)
+                                      ? Image.file(
+                                          bannerFile!,
+                                        )
+                                      : (community.banner.isEmpty ||
+                                              community.banner ==
+                                                  AssetsConstants.bannerDefault)
+                                          ? const Center(
+                                              child: Icon(
+                                              Icons.camera_alt_outlined,
+                                              size: 40,
+                                            ))
+                                          : Image.network(community.banner),
+                                )),
+                          ),
+                          Positioned(
+                            bottom: 20,
+                            left: 20,
+                            child: GestureDetector(
+                              onTap: selectProfileImage,
+                              child: (profileFile != null)
+                                  ? CircleAvatar(
+                                      backgroundImage: FileImage(profileFile!),
+                                      radius: 32,
+                                    )
+                                  : CircleAvatar(
+                                      backgroundImage:
+                                          NetworkImage(community.avatar),
+                                      radius: 32,
+                                    ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
         error: (error, stackTrace) => ErrorText(error: error.toString()),
