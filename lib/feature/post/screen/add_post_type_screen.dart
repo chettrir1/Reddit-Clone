0db9095af99787c2ad6e696b6abda2cccd335f8e
Raju Chettri <dada@Rajus-MacBook-Pro.local>
2024-03-09 18:23:34 -0500

    add post type and add post screen created

---
+++ lib/feature/post/screen/add_post_type_screen.dart
@@ -0,0 +1,168 @@
+import 'dart:io';
+
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/error_text.dart';
+import 'package:reddit_clone/core/common/loader.dart';
+import 'package:reddit_clone/core/utils.dart';
+import 'package:reddit_clone/feature/community/controller/community_controller.dart';
+import 'package:reddit_clone/models/community_model.dart';
+import 'package:reddit_clone/theme/palette.dart';
+
+class AddPostTypeScreen extends ConsumerStatefulWidget {
+  final String type;
+
+  const AddPostTypeScreen({super.key, required this.type});
+
+  @override
+  ConsumerState createState() => _AddPostTypeScreenState();
+}
+
+class _AddPostTypeScreenState extends ConsumerState<AddPostTypeScreen> {
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final linkController = TextEditingController();
+  File? bannerFile;
+  List<CommunityModel> communities = [];
+  CommunityModel? selectedCommunity;
+
+  void selectBannerImage() async {
+    final result = await pickImage();
+    if (result != null) {
+      setState(() {
+        bannerFile = File(result.files.first.path ?? "");
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+    descriptionController.dispose();
+    linkController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isTypeImage = (widget.type == "image");
+    final isTypeText = (widget.type == "text");
+    final isTypeLink = (widget.type == "link");
+    final currentTheme = ref.watch(themeNotifierProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Post ${widget.type}"),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "Share",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, color: Palette.blueColor),
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                  filled: true,
+                  hintText: "Enter title here",
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.blue),
+                      borderRadius: BorderRadius.circular(10)),
+                  border: InputBorder.none,
+                  contentPadding: const EdgeInsets.all(18)),
+              maxLength: 30,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            if (isTypeImage)
+              GestureDetector(
+                onTap: selectBannerImage,
+                child: DottedBorder(
+                    radius: const Radius.circular(4),
+                    dashPattern: const [10, 4],
+                    strokeCap: StrokeCap.round,
+                    borderType: BorderType.RRect,
+                    color: currentTheme.textTheme.bodyMedium!.color!,
+                    child: Container(
+                      width: double.infinity,
+                      height: 150,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: (bannerFile != null)
+                          ? Image.file(
+                              bannerFile!,
+                            )
+                          : const Center(
+                              child: Icon(
+                              Icons.camera_alt_outlined,
+                              size: 40,
+                            )),
+                    )),
+              ),
+            if (isTypeText)
+              TextField(
+                controller: descriptionController,
+                decoration: InputDecoration(
+                    filled: true,
+                    hintText: "Enter description here",
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.blue),
+                        borderRadius: BorderRadius.circular(10)),
+                    border: InputBorder.none,
+                    contentPadding: const EdgeInsets.all(18)),
+                maxLines: 5,
+              ),
+            if (isTypeLink)
+              TextField(
+                  controller: linkController,
+                  decoration: InputDecoration(
+                      filled: true,
+                      hintText: "Enter link here",
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.blue),
+                          borderRadius: BorderRadius.circular(10)),
+                      border: InputBorder.none,
+                      contentPadding: const EdgeInsets.all(18))),
+            const SizedBox(height: 20),
+            const Align(
+                alignment: Alignment.topLeft, child: Text("Select Community")),
+            ref.watch(userCommunitiesProvider).when(
+                data: (data) {
+                  communities = data;
+                  if (data.isEmpty) {
+                    return const SizedBox();
+                  }
+                  return DropdownButton(
+                      value: selectedCommunity ?? data[0],
+                      items: data
+                          .map((e) => DropdownMenuItem(
+                              value:e,
+                              child: Text(e.name)))
+                          .toList(),
+                      onChanged: (val) {
+                        setState(() {
+                          selectedCommunity = val;
+                        });
+                      });
+                },
+                error: (error, stackTrace) =>
+                    ErrorText(error: error.toString()),
+                loading: () => Loader())
+          ],
+        ),
+      ),
+    );
+  }
+}
