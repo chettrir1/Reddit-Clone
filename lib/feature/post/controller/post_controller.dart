a4e5e9a38c99bfea1f9265f3d956a987ca09a846
Raju Chettri <dada@Rajus-MacBook-Pro.local>
2024-03-13 11:37:24 -0400

    uuid library added

--- lib/feature/post/controller/post_controller.dart
+++ lib/feature/post/controller/post_controller.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/providers/storage_repository_provider.dart';
+import 'package:reddit_clone/feature/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/feature/post/repository/post_repository.dart';
+import 'package:reddit_clone/models/community_model.dart';
+import 'package:uuid/uuid.dart';
 
 class PostController extends StateNotifier<bool> {
   final PostRepository _postRepository;
@@ -15,4 +19,16 @@ class PostController extends StateNotifier<bool> {
         _ref = ref,
         _storageRepository = storageRepository,
         super(false);
+
+  void shareTextPost({
+    required BuildContext context,
+    required String title,
+    required CommunityModel selectedCommunity,
+    required String description,
+  }) {
+    state = true;
+    String postId = const Uuid().v1();
+    final user = _ref.read(userProvider);
+
+  }
 }
